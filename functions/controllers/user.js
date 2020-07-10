bb8ea747b316fec8f7d575e44794902749acba22
Carlo Bilbao <cbilbao88@gmail.com>
2020-07-09 18:35:47 -0700

    update user bug fix

--- functions/controllers/user.js
+++ functions/controllers/user.js
@@ -27,23 +27,19 @@ exports.updateUserData = async (req, res, next) => {
     // extract body data
     const user = req.body.user;
     const uid = req.body.uid;
-
-    // update min user data
-    if(user & user.name && user.name.value)
-        const test = await db.collection('usersMin').doc(uid).update({ name: user.name.value });
         
-    // update max user data
     try {
+        // update min user data
+        if(user & user.name && user.name.value)
+            await db.collection('usersMin').doc(uid).update({ name: user.name.value });
+
+        // update max user data
         const doc = await db.collection('usersMax').doc(uid).set(user);
-        if (doc.exists) {
-            const user = doc.data();
-            return res.status(200).json(user);
-        }
+        const updatedUser = doc.data();
+        return res.status(200).json(updatedUser);
     } catch(err){
         console.log("Error adding user");
         if (!err.statusCode) err.statusCode = 500;
         return next(err);
     }
-
-    return null;
 }
